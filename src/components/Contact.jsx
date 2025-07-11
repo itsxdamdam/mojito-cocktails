490f93b6b65fd6f14175e8c7876d2a1a46c1b2e4
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2025-07-11 17:08:58 +0100

    implement menu section

---
+++ src/components/Contact.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <footer id="contact">
+      <img
+        src="/images/footer-right-leaf.png"
+        alt="leaf-right"
+        id="f-right-leaf"
+      />
+      <img
+        src="/images/footer-left-leaf.png"
+        alt="leaf-left"
+        id="f-left-leaf"
+      />
+
+      <div className="content">
+        <h2>Where to Find Us</h2>
+      </div>
+    </footer>
+  );
+};
+
+export default Contact;
